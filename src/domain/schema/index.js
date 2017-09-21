7c476e7fda08789a03abde30af6806e0a9019da1
Chryb <chryb.q@gmail.com>
2017-09-21 12:16:16 +0200

    make executable schema

--- src/domain/schema/index.js
+++ src/domain/schema/index.js
@@ -1,5 +1,23 @@
 import query from './query.gql'
+import { makeExecutableSchema } from 'graphql-tools'
 
-console.log(query)
+//console.log(query)
 
-export default query
+const me = 5
+
+const resolvers = {
+  Query: {
+    me () {
+      return me
+    }
+  }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs: query,
+  resolvers
+})
+
+//console.log(schema)
+
+export default schema
